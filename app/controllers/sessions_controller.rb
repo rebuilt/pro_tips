e28081b256be31377588ed8b10e446fb581ec0f8
rebuilt <memoryman51@hotmail.com>
2025-03-13 15:37:41 -0700

    feat: allow user to 'sign in' with an email.  No password required

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,24 @@
 class SessionsController < ApplicationController
-  def new
-    @user = User.new
-  end
+  def new; end
 
   def create
+    user = User.find_by(email: params[:email].downcase)
+
+    # if user.present? && user.authenticate(params[:password])
+    if user.present? 
+      session[:user_id] = user.id
+      redirect_to account_tips_path
+    else
+      flash[:alert] = "Email or password were invalid.  Please try again"
+      render "new"
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    @current_user = nil
+    @goal_user = nil
+    redirect_to root_path
   end
 end
+
